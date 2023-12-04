4dbbb8e3d80547f065a87a26a59d44eabf5dc1a7
DolicaAkelloEgwel <d.akelloegwel@lcc.arts.ac.uk>
2023-12-04 15:43:50 +0000

    commands for wheel directions

--- main.py
+++ main.py
@@ -1,13 +1,52 @@
 from machine import Pin
-from time import sleep
+import time
 
-led = Pin("LED", machine.Pin.OUT)
-a = 0
+motor1a = Pin(14, Pin.OUT)
+motor1b = Pin(15, Pin.OUT)
 
-while True:
-    led.toggle()
-    sleep(1)
-    a += 20
-    b = a * 3
-    print(a, b)
+motor2a = Pin(10, Pin.OUT)
+motor2b = Pin(11, Pin.OUT)
+
+print("Hello.")
+
+def right_forward():
+    motor1a.value(0)
+    motor1b.value(0)
+    motor2a.value(0)
+    motor2b.value(1)
+
+def left_forward():
+    motor2a.value(0)
+    motor2b.value(0)
+    motor1a.value(1)
+    motor1b.value(0)
+
+def right_backward():
+    motor1a.value(0)
+    motor1b.value(0)
+    motor2a.value(1)
+    motor2b.value(0)
+
+def left_backward():
+    motor2a.value(0)
+    motor2b.value(0)
+    motor1a.value(0)
+    motor1b.value(1)
     
+def forward():
+    motor1a.value(1)
+    motor1b.value(0)
+    motor2a.value(0)
+    motor2b.value(1)
+    
+def backward():
+    motor1a.value(0)
+    motor1b.value(1)
+    motor2a.value(1)
+    motor2b.value(0)
+    
+def stop():
+    motor1a.value(0)
+    motor1b.value(0)
+    motor2a.value(0)
+    motor2b.value(0)
